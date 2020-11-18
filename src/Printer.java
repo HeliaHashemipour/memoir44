3c2a3c7149fa0891df02db0597889da4cbbb39d9
Helia Hashemipour <heliahashemipour2@gmail.com>
2020-11-18 11:04:00 +0330

    printer

--- src/Printer.java
+++ src/Printer.java
@@ -3,12 +3,12 @@
  */
 public interface Printer {
 
-    String[] colors = {"\u001B[37m", "\u001B[30m"};
+    String[] colors = {"\u001B[37m", "\u001B[30m"}; //White & Black
 
-    String AXIS = "\u001B[32m";
-    String ALLIED = "\u001B[31m";
+    String AXIS = "\u001B[33m"; // Yellow
+    String ALLIED = "\u001B[31m";//Red
 
-    String RESET = "\u001B[0m";
+    String RESET = "\u001B[0m"; //Reset
 
     static String getColor(int index) {
         return colors[index % 2];
